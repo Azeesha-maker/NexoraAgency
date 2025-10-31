93973c90868845b00fa59a4a7fcdc020b2541195
Azeesha-maker <azeeshaali01@gmail.com>
2025-10-31 14:27:25 +0500

    mobile

--- src/app/components/Layout/Header/index.tsx
+++ src/app/components/Layout/Header/index.tsx
@@ -91,7 +91,7 @@ const Header = () => {
         <div className='p-6'>
           <Logo />
           <nav className='flex flex-col space-y-6 mt-10 text-lg'>
-           <Link href="/site/services" className="hover:text-primary transition">
+           <Link href="/services" className="hover:text-primary transition">
   Services
 </Link>
 
