86225ad58fc45695de8331fbc32e88437629fc18
Azeesha-maker <azeeshaali01@gmail.com>
2025-11-01 01:17:35 +0500

    agency name changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ import Insta from '@/app/components/Home/Insta'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Desgy Solutions',
+  title: 'Nexora Agency',
 }
 
 export default function Home() {
