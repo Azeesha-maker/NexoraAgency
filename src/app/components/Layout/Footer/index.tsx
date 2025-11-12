128d24d78bb1fc057ef005422b9166e1a31a21cd
mrsaqi44 <mrzee3661@gmail.com>
2025-11-12 19:52:13 +0500

    update-code

--- src/app/components/Layout/Footer/index.tsx
+++ src/app/components/Layout/Footer/index.tsx
@@ -25,11 +25,17 @@ const Footer = () => {
 
   return (
     <div className='bg-black' id='first-section'>
-<div className='container mx-auto max-w-2xl pt-[25px] pb-16 px-4 sm:px-6 lg:max-w-7xl lg:px-8'>
+      <div className='container mx-auto max-w-2xl pt-[25px] pb-16 px-4 sm:px-6 lg:max-w-7xl lg:px-8'>
         <div className='grid grid-cols-1 gap-y-10 gap-x-16 sm:grid-cols-2 lg:grid-cols-12 xl:gap-x-8'>
           {/* COLUMN-1 */}
           <div className='col-span-4'>
-            <img src='/images/bg.png' alt='Nexora Agency Logo' className='h-30 w-35 mb-6 rounded-full' />
+            <Image
+              src="/images/bg.png"
+              alt="Nexora Agency Logo"
+              width={140}
+              height={140}
+              className="mb-6 fill-white"
+            />
             <div className='flex items-center gap-4'>
               <div className='footer-icons'>
                 <Link href='https://facebook.com'>
